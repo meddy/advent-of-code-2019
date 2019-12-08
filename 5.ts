4700fd9959c2118d2bd88f3426bf2d8da67801c0
Max Eddy <maxiscool@gmail.com>
2019-12-08 13:14:46 -0800

    Day 5 part one.

---
+++ 5.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+
+const ID = 1;
+
+function run(initial: number[]): void {
+  const program = [...initial];
+
+  let i = 0;
+  while (i < program.length) {
+    let opcode = program[i];
+    if (opcode === 3) {
+      const parameter = program[i + 1];
+      program[parameter] = ID;
+      i += 2;
+    } else {
+      const value = String(opcode);
+      opcode = Number(value.slice(-2));
+      const modes = value
+        .slice(0, value.length - 2)
+        .split("")
+        .reverse()
+        .map(Number);
+
+      switch (opcode) {
+        case 99:
+          return;
+        case 1: {
+          const parameters = program.slice(i + 1, i + 4);
+          const value1 =
+            modes[0] === 1 ? parameters[0] : program[parameters[0]];
+          const value2 =
+            modes[1] === 1 ? parameters[1] : program[parameters[1]];
+          program[parameters[2]] = value1 + value2;
+          i += 4;
+          break;
+        }
+        case 2: {
+          const parameters = program.slice(i + 1, i + 4);
+          const value1 =
+            modes[0] === 1 ? parameters[0] : program[parameters[0]];
+          const value2 =
+            modes[1] === 1 ? parameters[1] : program[parameters[1]];
+          program[parameters[2]] = value1 * value2;
+          i += 4;
+          break;
+        }
+        case 4: {
+          const parameter = program[i + 1];
+          console.log(program[parameter]);
+          i += 2;
+          break;
+        }
+        default:
+          throw new Error(`Unknown opcode ${opcode}`);
+      }
+    }
+  }
+}
+
+const content = fs.readFileSync(process.argv[2], "utf8");
+const program = content.split(",").map(Number);
+run(program);
